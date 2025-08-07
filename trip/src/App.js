b0dc2b83d3c3b11a20df8a332253af6a26fdfecc
achrefmechergui <achreffcb123achref@gamil.com>
2025-08-07 19:55:30 +0100

    trip-project

---
+++ trip/src/App.js
@@ -0,0 +1,88 @@
+import "./index.css";
+
+const initialItems = [
+  { id: 1, description: "Passport", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+
+];
+
+function Logo() {
+  return (
+    <div className="logo-container">
+      <h1> 🌴 Far Away 👜</h1>
+    </div>
+  );
+}
+
+function Form() {
+
+  function handleSubmit (e){
+    e.preventDefault()
+    console.log("test")
+  }
+  return (
+    <div className="form-container"  >
+      <h3> What do you need for your 😍 trip ? </h3>
+    <form className="form" onSubmit={handleSubmit}>
+        <select>
+         {Array.from({length : 20 } , (_,i)=> i+1 ).map((num)=>
+        <option value={num} key={num}>
+            {num}
+        </option>
+        ) }
+        </select>
+        <input type="text" placeholder="item ... "/>
+        <button  >Add</button>
+    </form>
+    </div>
+  );
+}
+
+function PackingList() {
+  return (
+    <div className="list ">
+      {initialItems.map((ele) => {
+        return (
+          
+            <Item item={ele} key={ele.id} />
+          
+        );
+      })}
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <span className="item" >
+      <span style={item.packed ? {textDecoration:"line-through"} : {}}>
+      
+        {item.quantity} {item.description} 
+      </span>
+      <button>❌</button>
+    </span>
+  );
+}
+
+function Stats() {
+  return (
+    <footer>
+      <strong>
+        👜 You have X items on your list , and you already packed X (X%)
+      </strong>
+    </footer>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+export default App;
